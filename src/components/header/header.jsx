819bc6156f7667f10c0e27ee05b9b37f0581dbbc
PiterPaper <piterpaper@yandex.ru>
2022-04-15 15:18:51 +0300

    master

---
+++ src/components/header/header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <section class="main__header">
+        <section class="main__header-wrap">
+          <span class="main__header-logo">SomeList</span>
+          <div class="main__header-group-lnk">
+            <a href="#" class="main__header-lnk lnk-active">События</a>
+            <a href="#" class="main__header-lnk">Архив</a>
+          </div>
+        </section>
+      </section>
+    )
+}
+
